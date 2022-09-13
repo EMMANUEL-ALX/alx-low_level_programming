e2ef85ab065101ad7ff408e693e3598fcc452ee2
EMMANUEL-ALX <emmanuelanaedobe@gmail>
2022-09-13 12:42:22 -0700

    task1-2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+                                                  /**
+ * main - Entry point
+ *
+ *Return :0on success always
+ */
+                                                   int main(void)                                    {
+          char emma [8] = "_putchar";
+          int i;
+
+
+          for  (i=0; i < 8; i++)
+  {
+                  _putchar(emma([i]);
+}
+_putchar('\n');
+
+          return (0);
+}
