6fb0f4d236f13e2739250e4f0f0afec62debb797
EMMANUEL-ALX <emmanuelanaedobe@gmail>
2022-09-13 14:03:11 -0700

    4-isa

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isalpha - check for lower case letter
- * @c : character to check
- * Return:0 or 1
+ *_isalpha - check for lower case letter
+ *@c : character to check
+ *Return:0 or 1
  */
 
 int _isalpha(int c)
