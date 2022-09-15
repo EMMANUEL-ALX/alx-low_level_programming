ea1b9cd693c76c1d005cb5655abafbf2c1d8281d
EMMANUEL-ALX <emmanuelanaedobe@gmail>
2022-09-15 15:19:17 -0700

    5task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+
+
+#include "main.h"
+
+/**
+  * more_numbers - Prints 10 time from 0-14
+  * Description: Only use _putchar three times
+  * Return: Void
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
